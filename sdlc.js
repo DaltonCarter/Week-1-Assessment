ea08f7b883514932131d7852f82b84855a1c0bda
Dalton Carter <DJ_Carter_Gottenbos@yahoo.com>
2022-10-21 09:15:50 -0700

    End of SDLC portion.

---
+++ sdlc.js
@@ -0,0 +1,37 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "This second step is where we determine what the user expects or needs from a new or modified product. For best effect these requirements should be Documented, actionable, measurable, testable, traceable, and defined to a level sufficient for System Design."
+
+const Design = "This is where the modeling of how  the product will work takes place. It can include several steps such as; UI: how the users interact with the product and how it will respond to certain inputs, Programming: The language used to program the product., etc..."
+
+const Implementation = "This is where the the development team creates the product, obviously this is where the actual building and coding of the product begins."
+
+const testingAndIntegration = "This phase is where the pieces are brought together, and combined and tested to ensure the product is going to work as intended and with minimal bugs."
+
+const maintain = "The maintenance phase is where anything that slipped past the testing phase before release geets discovered and resolved. This is frequently an ongoing process."
+
