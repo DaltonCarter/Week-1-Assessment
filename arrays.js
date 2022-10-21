ad077ecf0fa54ff776416c0b92c3b60831758711
Dalton Carter <DJ_Carter_Gottenbos@yahoo.com>
2022-10-21 09:33:02 -0700

    Arrays finished.

--- arrays.js
+++ arrays.js
@@ -5,11 +5,15 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+let colorCopy = faveColors.slice(0, 3)
+console.log(colorCopy)
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
 
+colorCopy.push('Orange')
+console.log(colorCopy)
 //////////////////PROBLEM 3////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
@@ -20,7 +24,8 @@ const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
 
-
+let middleNums = numbers.splice(1, 3)
+console.log(middleNums)
 //////////////////PROBLEM 4////////////////////
 /* 
   Create an empty array called 'answers'.
@@ -34,3 +39,15 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+
+let answers = []
+
+for(let i = 0; i < bigOrSmallArray.length; i++){
+  if(bigOrSmallArray[i] === 100 || bigOrSmallArray[i] < 100){
+    answers.push('Small')
+  } else {
+    answers.push('Big')
+  }
+  
+}
+console.log(answers)
